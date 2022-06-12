2d14b445523b6dcabea113d495356d50539328a5
sonlv <son.le@nfq.asia>
2022-06-12 22:28:02 +0700

    add post model

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,4 +1,4 @@
-import {Link, NavLink, routes} from '@redwoodjs/router'
+import { Link, NavLink, routes } from '@redwoodjs/router'
 
 type BlogLayoutProps = {
   children?: React.ReactNode
@@ -13,10 +13,14 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
         </h1>
         <ul className="flex font-semibold text-xl">
           <li className="mr-2">
-            <NavLink to={routes.home()} activeClassName="text-blue-500">Home</NavLink>
+            <NavLink to={routes.home()} activeClassName="text-blue-500">
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to={routes.about()} activeClassName="text-blue-500">About</NavLink>
+            <NavLink to={routes.about()} activeClassName="text-blue-500">
+              About
+            </NavLink>
           </li>
         </ul>
       </nav>
